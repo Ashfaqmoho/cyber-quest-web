3c485dd3be494b5cbfe3e772dc78c90c9f7853bc
Mohomad Ashfaq <ashfaqmohamed411@gmail.com>
2025-03-24 07:17:10 +0530

    change the user validation

--- script.js
+++ script.js
@@ -30,6 +30,16 @@ document
       return;
     }
 
+    if (isNumericOnly(name) || isNumericOnly(username)) {
+      alert("Name and Username cannot be numbers only!");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("Password must be at least 6 characters long!");
+      return;
+    }
+
     if (password !== confirmPassword) {
       alert("Passwords do not match!");
       return;
